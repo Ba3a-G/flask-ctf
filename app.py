a281942cb44f1a19fb754afc740b5e0aee45f55f
Aryan Kumar <aryan.ak434@gmail.com>
2023-04-07 10:49:31 +0530

    add: interactive debugger

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from werkzeug.debug import DebuggedApplication
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def index():
-  return 'Hello from LPU!'
+    raise Exception('Okayy! Can you hack the console now?')
+    return 'Hello from LPU!'
 
 if __name__ == '__main__':
     app.run(debug=True)
